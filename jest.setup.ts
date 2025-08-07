613675ee763e3f4513fb64e9db01afa7b63da0d7
Artur trombim <arturtrombim.dev@gmail.com>
2025-08-07 09:23:08 -0300

    chore: adicionar configurações necessárias para testes unitários

---
+++ jest.setup.ts
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+
+// Mock para window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,          // você pode alterar para true se quiser testar mobile
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),  // deprecated, mas algumas libs ainda usam
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+})
+
+global.ResizeObserver = class ResizeObserver {
+  observe() { }
+  unobserve() { }
+  disconnect() { }
+};
