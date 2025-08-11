816ccceb9339417c4f118af6b96e40732ecd92ee
Artur trombim <arturtrombim.dev@gmail.com>
2025-08-10 21:48:10 -0300

    test: criar teste para validar componente

---
+++ __tests__/components/ver-mais-informacao.spec.tsx
@@ -0,0 +1,26 @@
+import { VerMaisInformacao } from "@/components/ver-mais-informacao"
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+describe("ver mais informações", () => {
+    const tituloMock = "Teste"
+    const descricaoMock = "Teste descricao"
+    const setup = () => {
+        render(<VerMaisInformacao titulo={tituloMock} descricao={descricaoMock} />)
+    }
+    it("renderiza apenas o ícone inicialmente", () => {
+        setup();
+
+        expect(screen.getByLabelText(/ver mais informações/i)).toBeInTheDocument()
+        expect(screen.queryByLabelText(/conteudo informações/i)).not.toBeInTheDocument()
+    })
+    it("abre o diálogo ao clicar no ícone", async () => {
+        const user = userEvent.setup()
+        setup()
+
+        const trigger = screen.getByLabelText(/ver mais informações/i);
+        await user.click(trigger)
+
+        expect(screen.queryByLabelText(/conteudo informações/i)).toBeInTheDocument()
+    })
+})
